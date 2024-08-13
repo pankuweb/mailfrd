db5d33aea780b23e399a74a6ad74158bc04ab90e
Pankaj Kumar <jakharsahil95@gmail.com>
2024-08-13 16:39:52 +0530

    Worked on background task

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,39 +1,150 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   StyleSheet,
   TouchableOpacity,
   PermissionsAndroid,
+  Button,
+  Platform,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {useNavigation} from '@react-navigation/native';
+import BackgroundService from 'react-native-background-actions';
+import SmsListener from 'react-native-get-sms-android';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [messages, setMessages] = useState([]);
+  const [showMessages, setShowMessages] = useState(false);
+  const [RecipientNumber, setRecipientNumber] = useState('');
+
+  useEffect(() => {
+    const getRecipientNumber = async () => {
+      try {
+        const storedItems = await AsyncStorage.getItem('messageData');
+        if (storedItems) {
+          setRecipientNumber(JSON.parse(storedItems)?.[0]?.recipientValue);
+        }
+      } catch (error) {
+        console.error('Failed to retrieve recipient number:', error);
+      }
+    };
+
+    getRecipientNumber();
+  }, []);
+
+  const forwardSms = () => {
+    const filter = {
+      box: 'inbox',
+      maxCount: 1, // Fetch the most recent message
+    };
+
+    SmsListener.list(
+      JSON.stringify(filter),
+      fail => {
+        console.error('Failed with this error: ' + fail);
+      },
+      (count, smsList) => {
+        try {
+          const arr = JSON.parse(smsList);
+          console.log('Count: ', count);
+          console.log('List: ', arr);
+
+          setMessages(arr); // Store the messages in the state
+          setShowMessages(true); // Trigger UI update to show messages
+
+          if (arr.length > 0) {
+            sendMessage(arr[0].body);
+          }
+        } catch (error) {
+          console.error('Error parsing SMS list:', error);
+        }
+      },
+    );
+  };
+  const sendMessage = (messageContent) => {
+    SmsListener.autoSend(
+      RecipientNumber, // Use the actual recipient number from state
+      messageContent,
+      fail => {
+        console.error('Failed to send SMS: ' + fail);
+      },
+      success => {
+        console.log('SMS sent successfully');
+      },
+    );
+  };
+
+  const startBackgroundJob = async () => {
+    const veryIntensiveTask = async taskDataArguments => {
+      const {delay} = taskDataArguments;
+      while (BackgroundService.isRunning()) {
+        console.log('Task is running...');
+        forwardSms();
+        await sleep(delay);
+      }
+    };
+
+    const options = {
+      taskName: 'ConsoleLogger',
+      taskTitle: 'Background Task',
+      taskDesc: 'Running a task every 5 seconds',
+      taskIcon: {
+        name: 'ic_launcher',
+        type: 'mipmap',
+      },
+      color: '#ff00ff',
+      linkingURI: 'yourSchemeHere://chat/jane',
+      parameters: {
+        delay: 5000, // 5 seconds
+      },
+    };
+
+    try {
+      if (BackgroundService.isRunning()) {
+        console.log('Background service is already running');
+        return;
+      }
+      await BackgroundService.start(veryIntensiveTask, options);
+      console.log('Background service started');
+    } catch (error) {
+      console.error('Failed to start background service:', error);
+    }
+  };
+
+  const stopBackgroundJob = async () => {
+    try {
+      if (!BackgroundService.isRunning()) {
+        console.log('Background service is not running');
+        return;
+      }
+      await BackgroundService.stop();
+      console.log('Background service stopped');
+    } catch (error) {
+      console.error('Failed to stop background service:', error);
+    }
+  };
+
   async function requestSmsPermissions() {
     try {
       const granted = await PermissionsAndroid.requestMultiple([
         PermissionsAndroid.PERMISSIONS.RECEIVE_SMS,
         PermissionsAndroid.PERMISSIONS.READ_SMS,
         PermissionsAndroid.PERMISSIONS.SEND_SMS,
+        // PermissionsAndroid.PERMISSIONS.FOREGROUND_SERVICE,
       ]);
       console.log(granted, 'You can receive and send SMS');
-
-      // if (
-      //   granted['android.permission.RECEIVE_SMS'] === 'granted' &&
-      //   granted['android.permission.READ_SMS'] === 'granted' &&
-      //   granted['android.permission.SEND_SMS'] === 'granted'
-      // ) {
-      //   console.log(granted,'You can receive and send SMS');
-      // } else {
-      //   console.log('SMS permission denied');
-      // }
     } catch (err) {
       console.warn(err);
     }
   }
+
   useEffect(() => {
-    requestSmsPermissions();
+    if (Platform.OS === 'android') {
+      requestSmsPermissions();
+    }
   }, []);
 
   return (
@@ -43,6 +154,8 @@ const HomeScreen = () => {
         onPress={() => navigation.navigate('FilterScreen')}>
         <Icon name="plus" size={30} color="#fff" />
       </TouchableOpacity>
+      <Button title="Start Background Job" onPress={startBackgroundJob} />
+      <Button title="Stop Background Job" onPress={stopBackgroundJob} />
     </View>
   );
 };
@@ -67,7 +180,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.8,
     shadowRadius: 5,
     elevation: 5,
-    textAlign: 'center',
     alignItems: 'center',
   },
 });
--- src/screens/messages/summary.tsx
+++ src/screens/messages/summary.tsx
@@ -1,69 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, DeviceEventEmitter } from 'react-native';
-import SmsListener from 'react-native-get-sms-android';
-import { useRoute } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useRoute, CommonActions } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SummaryScreen = ({ navigation }) => {
-  const [messages, setMessages] = useState([]);
-  const [showMessages, setShowMessages] = useState(false);
-
   const route = useRoute();
   const { recipientType, recipientValue, keywords } = route.params?.data || {};
-
-  useEffect(() => {
-    // Set up the listener for incoming SMS
-    const subscription = DeviceEventEmitter.addListener('sms_received', (sms) => {
-      console.log('SMS received:', sms);
-      forwardSms();
-      setShowMessages(true);
-    });
-
-    return () => {
-      // Clean up the subscription when the component is unmounted
-      subscription.remove();
-    };
-  }, []);
-
-  function forwardSms() {
-    const filter = {
-      box: 'inbox',
-      maxCount: 1, // Fetch the most recent message
-    };
-
-    SmsListener.list(
-      JSON.stringify(filter),
-      fail => {
-        console.log('Failed with this error: ' + fail);
-      },
-      (count, smsList) => {
-        console.log('Count: ', count);
-        const arr = JSON.parse(smsList);
-        console.log('List: ', arr);
-
-        setMessages(arr); // Store the messages in the state
-        setShowMessages(true); // Trigger UI update to show messages
-
-        // Automatically send the most recent message
-        if (arr.length > 0) {
-          sendMessage(arr[0].address, arr[0].body);
-        }
-      },
-    );
-  }
-
-  const sendMessage = (recipientNumber, messageContent) => {
-    SmsListener.autoSend(
-      `+91${recipientValue}`, // Replace with the actual recipient number
-      messageContent,
-      fail => {
-        console.log('Failed with this error: ' + fail);
-      },
-      success => {
-        console.log('SMS sent successfully');
-      },
-    );
-  };
 
   const saveDataToLocalStorage = async () => {
     try {
@@ -76,7 +18,6 @@ const SummaryScreen = ({ navigation }) => {
         recipientType,
         recipientValue,
         keywords: keywords || [],
-        messages: messages || [],
       };
 
       // Add the new entry to the existing array
@@ -92,18 +33,15 @@ const SummaryScreen = ({ navigation }) => {
   };
 
   const handleDone = async () => {
-    await forwardSms();
     await saveDataToLocalStorage();
-    // Optionally navigate to another screen
-    // navigation.navigate('HomeScreen');
+    // Reset the navigation stack and navigate to HomeScreen
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [{ name: 'HomeScreen' }],
+      })
+    );
   };
-
-  const renderItem = ({ item }) => (
-    <View style={styles.messageItem}>
-      <Text style={styles.messageDate}>{new Date(item.date).toLocaleString()}</Text>
-      <Text style={styles.messageBody}>{item.body}</Text>
-    </View>
-  );
 
   return (
     <View style={styles.container}>
@@ -117,18 +55,10 @@ const SummaryScreen = ({ navigation }) => {
       </View>
       <TouchableOpacity
         style={styles.doneButton}
-        onPress={handleDone} // Call handleDone when pressed
+        onPress={handleDone}
       >
         <Text style={styles.doneButtonText}>Done</Text>
       </TouchableOpacity>
-      {/* {showMessages && (
-        <FlatList
-          data={messages}
-          renderItem={renderItem}
-          keyExtractor={(item) => item._id.toString()}
-          style={styles.messageList}
-        />
-      )} */}
     </View>
   );
 };
@@ -137,7 +67,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    // justifyContent: 'space-between',
   },
   section: {
     marginBottom: 20,
@@ -149,18 +78,6 @@ const styles = StyleSheet.create({
   },
   value: {
     fontSize: 16,
-  },
-  separator: {
-    height: 1,
-    backgroundColor: '#ccc',
-    marginVertical: 10,
-  },
-  phoneNumberContainer: {
-    marginBottom: 20,
-  },
-  phoneNumber: {
-    fontSize: 16,
-    fontWeight: 'bold',
   },
   doneButton: {
     backgroundColor: '#007bff',
@@ -176,21 +93,6 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
-  },
-  messageList: {
-    marginTop: 20,
-  },
-  messageItem: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  messageDate: {
-    fontSize: 14,
-    color: '#555',
-  },
-  messageBody: {
-    fontSize: 16,
   },
 });
 
